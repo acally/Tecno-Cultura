665ca6aebba513db374e8cffa554cc89d5e6eeee
acally <tsmec23@gmail.com>
2019-09-18 08:39:19 -0500

    tecno-cultura

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { ConocenosComponent } from './conocenos/conocenos.component';
+import { NAnterioresComponent } from './n-anteriores/n-anteriores.component';
+import { EditorialComponent } from './editorial/editorial.component';
+import { ContactanosComponent } from './contactanos/contactanos.component';
+import { LineamientosComponent } from './lineamientos/lineamientos.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    InicioComponent,
+    ConocenosComponent,
+    NAnterioresComponent,
+    EditorialComponent,
+    ContactanosComponent,
+    LineamientosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
